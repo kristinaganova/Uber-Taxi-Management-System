a579a6d260ff87fdb91872d7f43a01d25b62df60
kristinaganova <115446275+kristinaganova@users.noreply.github.com>
2023-06-10 00:03:04 +0300

    Update MessageManager.h
    make MessageManager a Singleton

--- MessageMenager/MessageManager.h
+++ MessageMenager/MessageManager.h
@@ -1,16 +1,29 @@
 #pragma once
+
 #include "Vector.hpp"
 #include "Message.h"
 
 class MessageManager
 {
     Vector<Message> messages;
+    static unsigned int nextId;
+
+    MessageManager();
+
+    MessageManager(const MessageManager& other) = delete;
+    MessageManager& operator=(const MessageManager& other) = delete;
+
+    MessageManager(MessageManager&& other) noexcept = delete;
+    MessageManager& operator=(MessageManager&& other) noexcept = delete;
 
 public:
+    static MessageManager& getInstance();
+
     void addMessage(const Message& message);
     void printUnreadMessages() const;
     void removeMessage(unsigned int messageId);
     void deleteAllMessages();
     void printAllMessages() const;
     Message* getMessageById(unsigned int messageId);
+    unsigned int getNextId() const;
 };
