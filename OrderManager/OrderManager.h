8408cb9af7624e316e4becafeecf294c8ce8263d
kristinaganova <115446275+kristinaganova@users.noreply.github.com>
2023-06-10 00:23:53 +0300

    Update OrderManager.h

--- OrderManager/OrderManager.h
+++ OrderManager/OrderManager.h
@@ -8,12 +8,15 @@ enum class Status;
 class OrderManager
 {
 	Vector<Order> orders;
+	OrderManager() {}
 
+	static OrderManager* instance;
 public:
-
+	static OrderManager* getInstance();
 	void addOrder(const Order& order);
 	void removeOrder(unsigned int orderId);
 	Order* findOrderById(unsigned int orderId);
 	Vector<Order> getOrdersByStatus(Status status);
 	void printAllOrders() const;
+	void notifyDriversInRange(const Order& order);
 };
